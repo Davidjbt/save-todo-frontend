e22f7c62bdfc04422c03878784fdd46d772a3f70
Davidjbt <101603373+Davidjbt@users.noreply.github.com>
2025-08-20 17:52:23 +0100

    feat(ui): add common layout with navbar
    - Added navbar component
    - Added common layout (RootLayout) and replaced Outlet in / route
    - Added styling to current components
    - Added JetBrains Mono font
    
    Refs #3

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import {createBrowserRouter, createRoutesFromElements, Outlet, Route, RouterProvider} from 'react-router-dom'
-
+import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from 'react-router-dom'
+import RootLayout from "./layouts/RootLayout.jsx";
 
 const App = () => {
     const routes = createBrowserRouter(
         createRoutesFromElements(
-            <Route path="/" element={<Outlet />}>
+            <Route path="/" element={<RootLayout />}>
                 <Route index element={<div>Home temp</div>} />
                 <Route path="sign-up" element={<div>Sign Up temp</div>}/>
                 <Route path="login" element={<div>Login</div>}/>
